e45313e7a5824b5a54783a416880958c8026d206
ElinaIR <elinaigdavletova@yandex.ru>
2023-05-30 13:58:15 +0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes.js'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/js/bootstrap.min.js'
+import store from './store'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router).use(store)
+app.mount('#app')
+
